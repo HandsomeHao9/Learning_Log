c1304f9e8a1a300c6ec1658646278b16f383e292
coamp <1033623396@qq.com>
2024-11-01 22:29:15 +0800

    插入图标

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -81,7 +81,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-    'https://coamp.fra1.zeabur.app/',
+    # 'https://coamp.fra1.zeabur.app/',
 )
 
 CSRF_TRUSTED_ORIGINS = ['https://coamp.fra1.zeabur.app']
@@ -140,7 +140,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-print(BASE_DIR)
 
 # My settings
 LOGIN_URL = 'users:login'
--- learning_log/urls.py
+++ learning_log/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles import views
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('learning_logs.urls')),
+    re_path(r'^static/(?P<path>.*)$', views.serve),
 ]
