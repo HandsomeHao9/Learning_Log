2866c2c700127dba94390e5172d28f195d670f43
coamp <1033623396@qq.com>
2024-11-01 20:41:21 +0800

    解决跨域

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -45,11 +45,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,6 +135,10 @@ LOGIN_URL = 'users:login'
 # Heroku settings...
 #import django_heroku
 #django_heroku.settings(locals())
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_HEADERS = ('*')
 
 if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
