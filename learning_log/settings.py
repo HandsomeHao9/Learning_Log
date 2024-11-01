c29b1867693090356bca6968b4c9a26d0194d376
coamp <1033623396@qq.com>
2024-11-01 20:49:39 +0800

    解决跨域

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -77,6 +77,35 @@ TEMPLATES = [
     },
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+	'*'
+)
+CORS_ALLOW_METHODS = (
+	'DELETE',
+	'GET',
+	'OPTIONS',
+	'PATCH',
+	'POST',
+	'PUT',
+	'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+	'XMLHttpRequest',
+	'X_FILENAME',
+	'accept-encoding',
+	'authorization',
+	'content-type',
+	'dnt',
+	'origin',
+	'user-agent',
+	'x-csrftoken',
+	'x-requested-with',
+	'Pragma',
+)
+
 WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 
